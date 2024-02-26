0153c922cdf29f568a6f6c10f7309a0e8f8a3dcb
tim.schouten <tim.schouten@student.rocva.nl>
2024-02-26 22:27:58 +0100

    Initial endpoints for testing purposes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::group(["prefix" => "event"], function() {
+    Route::GET("",[EventController::class, "index"]);
+    Route::GET("/{id}",[EventController::class, "show"]);
 });
